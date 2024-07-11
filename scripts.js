944a62803fbfa190c8a91e8c876cf7bbf6abeb14
MaksatDowletow <tdlipfmdm@outlook.com>
2024-07-12 00:02:12 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,3 +24,16 @@ function showSlides() {
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
+
+function setLanguage(lang) {
+  document.querySelectorAll('[data-lang]').forEach(element => {
+    if (element.dataset.lang === lang) {
+      element.hidden = false;
+    } else {
+      element.hidden = true;
+    }
+  });
+}
+
+// Установить язык по умолчанию (например, русский)
+setLanguage('tm');
