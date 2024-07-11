22f34a8ce375469e00b6d34699ecb1ecce425b48
MaksatDowletow <tdlipfmdm@outlook.com>
2024-07-11 23:37:12 +0500

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,1 +1,13 @@
 
+function setLanguage(lang) {
+  document.querySelectorAll('[data-lang]').forEach(element => {
+    if (element.dataset.lang === lang) {
+      element.hidden = false;
+    } else {
+      element.hidden = true;
+    }
+  });
+}
+
+// Установить язык по умолчанию (например, русский)
+setLanguage('ru');
