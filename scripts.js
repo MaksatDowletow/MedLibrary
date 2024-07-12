940d889fac3c1d8bcc3939aabdbaad4c536cd29c
MaksatDowletow <tdlipfmdm@outlook.com>
2024-07-12 22:43:28 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,7 +13,23 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 // Логика слайдера
 let slideIndex = 1;
 showSlides(slideIndex);
