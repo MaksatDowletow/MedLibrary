a121ecc1b8ced04ec63e6f55cc64c84ce9d35192
MaksatDowletow <tdlipfmdm@outlook.com>
2024-07-13 06:41:26 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ let slideIndex = 0;
       slideIndex++;
       if (slideIndex > slides.length) {slideIndex = 1}    
       slides[slideIndex-1].style.display = "block";  
-      setTimeout(showSlides, 2000); // Change image every 2 seconds
+      setTimeout(showSlides, 10000); // Change image every 10 seconds
     }
 
     function plusSlides(n) {
