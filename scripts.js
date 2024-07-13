c86cab9e38e1c17d2f27550a7a547860341411d3
MaksatDowletow <tdlipfmdm@outlook.com>
2024-07-13 06:40:43 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,23 +14,31 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 let slideIndex = 0;
+    showSlides();
 
-// Логика слайдера
-let slideIndex = 1;
-showSlides(slideIndex);
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("slide");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      slides[slideIndex-1].style.display = "block";  
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
 
-function showSlides(n) {
-  let i;
-  const slides = document.querySelectorAll('.slide');
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  slides.forEach(slide => slide.style.display = 'none');
-  slides[slideIndex - 1].style.display = 'block';
-}
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    document.addEventListener('DOMContentLoaded', (event) => {
+      showSlides(slideIndex);
+    });
 
 function sendMail() {
   const name = document.getElementById('name').value;
